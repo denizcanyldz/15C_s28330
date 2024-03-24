1131a3b12a9f43fe68e41875c62c323d3b18f1aa
s28330 <s28330@pjwstk.edu.pl>
2024-03-24 14:17:29 +0100

    Task 4: Libraries

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 squares = []
 for x in range(1, 11):
     squares.append(x ** 2)
@@ -16,18 +18,26 @@ def square_list(start, end):
 example = square_list(2, 6)
 print(example)
 
-# Task 3: Classes
+# Task 3: Classes and Task 4: Libraries
 class SquareGenerator:
     def __init__(self,start,end):
         self.start = start
         self.end = end
 
-    def generate_square(self):
-        squares3 = []
-        for z in range(self.start,self.end +1):
-            squares3.append(z**2)
-        return squares3
+    def generate_squares_and_roots(self):
+        squares_and_roots = []
+        for x in range(self.start, self.end + 1):
+            square = x**2
+            square_root = math.sqrt(square)
+            squares_and_roots.append((square, square_root))
+        return squares_and_roots
 
 # Let's write an example
 example = SquareGenerator(3,5)
-print(example.generate_square())
+generator = example.generate_squares_and_roots()
+print("Squares and Square Roots:")
+for square, root in generator:
+    print(f"Number: {square}, Square Root: {root:.2f}")
+
+
+
