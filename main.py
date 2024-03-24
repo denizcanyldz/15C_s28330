35d9f79daf24d50155c389340e7faefa3ee106b8
s28330 <s28330@pjwstk.edu.pl>
2024-03-24 14:06:11 +0100

    Task 2: Functions

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 squares = []
-for x in range(1,11):
-    squares.append(x**2)
+for x in range(1, 11):
+    squares.append(x ** 2)
 print(squares)
+
+
+# Task 2: Functions
+def square_list(start, end):
+    squares1 = []
+    for y in range(start, end + 1):
+        squares1.append(y ** 2)
+    return squares1
+
+
+# Let's write an example
+example = square_list(2, 6)
+print(example)
 
