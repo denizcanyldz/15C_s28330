732eeb898c566ca5fa6f8329c93d2da1eac7e64c
s28330 <s28330@pjwstk.edu.pl>
2024-03-24 14:11:09 +0100

    Task 3: Classes

--- main.py
+++ main.py
@@ -16,3 +16,18 @@ def square_list(start, end):
 example = square_list(2, 6)
 print(example)
 
+# Task 3: Classes
+class SquareGenerator:
+    def __init__(self,start,end):
+        self.start = start
+        self.end = end
+
+    def generate_square(self):
+        squares3 = []
+        for z in range(self.start,self.end +1):
+            squares3.append(z**2)
+        return squares3
+
+# Let's write an example
+example = SquareGenerator(3,5)
+print(example.generate_square())
